c196955693e26025ffd3820688fdd57bdf4d0d15
Hamid Yaftian <hamid@re-work.dev>
2022-11-01 16:58:37 +0330

    Use modal from deriv ui

--- packages/cashier/src/pages/payment-agent/payment-agent-signup/cancel-wizard-dialog/cancel-wizard-dialog.tsx
+++ packages/cashier/src/pages/payment-agent/payment-agent-signup/cancel-wizard-dialog/cancel-wizard-dialog.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dialog } from '@deriv/components';
+import { Modal } from '@deriv/ui';
 import { localize } from '@deriv/translations';
 
 type TCancelWizardDialogProps = {
@@ -8,19 +8,43 @@ type TCancelWizardDialogProps = {
     is_visible: boolean;
 };
 
+// TODO: These types should be imported from @deriv/ui
+type TButtonColor = 'primary' | 'primary-light' | 'secondary' | 'tertiary' | 'monochrome';
+export type TModalActionButton = {
+    id: number;
+    text: string;
+    color: TButtonColor;
+    onClick: () => void;
+};
+
 const CancelWizardDialog = ({ onConfirm, onCancel, is_visible }: TCancelWizardDialogProps) => {
+    const actionButtons: TModalActionButton[] = [
+        {
+            id: 0,
+            text: localize('No, take me back'),
+            color: 'tertiary',
+            onClick: onCancel,
+        },
+        {
+            id: 1,
+            text: localize('Yes, cancel'),
+            color: 'primary',
+            onClick: onConfirm,
+        },
+    ];
+
     return (
-        <Dialog
-            title={localize('Are you sure you want to cancel?')}
-            confirm_button_text={localize('Yes, cancel')}
-            cancel_button_text={localize('No, take me back')}
-            onConfirm={onConfirm}
-            onCancel={onCancel}
-            is_visible={is_visible}
-            has_close_icon
-        >
-            {localize('If you click Cancel, you’ll lose the inputs you’ve made so far.')}
-        </Dialog>
+        <Modal open={is_visible}>
+            <Modal.Portal>
+                <Modal.Overlay />
+                <Modal.DialogContent
+                    title={localize('Are you sure you want to cancel?')}
+                    content={localize('If you click Cancel, you’ll lose the inputs you’ve made so far.')}
+                    action_buttons={actionButtons}
+                    has_close_button
+                />
+            </Modal.Portal>
+        </Modal>
     );
 };
 
